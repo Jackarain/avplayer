81b5f921992db1d5212af34edf6881ad42e1fc72
microcai <microcai@fedoraproject.org>
2012-11-28 20:41:01 +0800

    linux port, structure done. now begin to fill-in code.
    forget to add sdl_render

---
+++ video/sdl_render.cpp
@@ -0,0 +1,84 @@
+/*
+    Copyright (C) 2012  microcai <microcai@fedoraproject.org>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+#define UINT64_C(c)     c ## ULL
+
+#include <avplay.h>
+
+#include "sdl_render.h"
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+EXPORT_API int sdl_init_video(void *ctx, int w, int h, int pix_fmt)
+{
+	vo_context *vo = (vo_context*)ctx;
+	sdl_render *sdl = NULL;
+	vo->video_dev = (void*)(sdl = new sdl_render);
+	return sdl->init_render(vo->user_data, w, h, pix_fmt) ? 0 : -1;
+}
+
+EXPORT_API int sdl_render_one_frame(void *ctx, AVFrame* data, int pix_fmt, double pts)
+{
+	vo_context *vo = (vo_context*)ctx;
+	sdl_render *sdl = (sdl_render*)vo->video_dev;
+	return sdl->render_one_frame(data, pix_fmt) ? 0 : -1;
+}
+
+EXPORT_API void sdl_re_size(void *ctx, int width, int height)
+{
+	vo_context *vo = (vo_context*)ctx;
+	sdl_render *sdl = (sdl_render*)vo->video_dev;
+	sdl->re_size(width, height);
+}
+
+EXPORT_API void sdl_aspect_ratio(void *ctx, int srcw, int srch, int enable_aspect)
+{
+	vo_context *vo = (vo_context*)ctx;
+	sdl_render *sdl = (sdl_render*)vo->video_dev;
+	sdl->aspect_ratio(srcw, srch, enable_aspect);
+}
+
+EXPORT_API int sdl_use_overlay(void *ctx)
+{
+	vo_context *vo = (vo_context*)ctx;
+	sdl_render *sdl = (sdl_render*)vo->video_dev;
+	return sdl->use_overlay() ? 0 : -1;
+}
+
+EXPORT_API void sdl_destory_render(void *ctx)
+{
+	vo_context *vo = (vo_context*)ctx;
+	sdl_render *sdl = (sdl_render*)vo->video_dev;
+	if (sdl)
+	{
+		sdl->destory_render();
+		delete sdl;
+		vo->video_dev = NULL;
+	}
+}
+
+#ifdef  __cplusplus
+}
+#endif
+
+
+bool sdl_render::init_render(void* ctx, int w, int h, int pix_fmt)
+{
+	
+}
---
+++ video/sdl_render.h
@@ -0,0 +1,44 @@
+/*
+    Copyright (C) 2012  microcai <microcai@fedoraproject.org>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+
+#ifndef SDL_RENDER_H
+#define SDL_RENDER_H
+
+#include "video_render.h"
+
+class sdl_render : public video_render
+{
+public:
+		// 初始render.
+	virtual bool init_render(void* ctx, int w, int h, int pix_fmt);
+
+	// 渲染一帧.
+	virtual bool render_one_frame(AVFrame* data, int pix_fmt){}
+
+	// 调整大小.
+	virtual void re_size(int width, int height){}
+
+	// 设置宽高比.
+	virtual void aspect_ratio(int srcw, int srch, bool enable_aspect){}
+
+	// 撤销render.
+	virtual void destory_render(){}
+};
+
+#endif // SDL_RENDER_H
