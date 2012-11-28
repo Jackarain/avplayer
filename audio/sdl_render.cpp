ef9178eab86d7c88c6b035120424fc0715589bc5
microcai <microcai@fedoraproject.org>
2012-11-28 23:59:40 +0800

    add sdl_audio_render

---
+++ audio/sdl_render.cpp
@@ -0,0 +1,97 @@
+/*
+    Copyright (C) 2012  microcai <microcai@fedoraproject.org>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+#define UINT64_C(c)     c ## ULL
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+#include <SDL/SDL_audio.h>
+#include <avplay.h>
+#include "sdl_render.h"
+
+#define EXPORT_API
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+EXPORT_API int sdl_init_audio(void* ctx, uint32_t channels,
+	uint32_t bits_per_sample, uint32_t sample_rate, int format)
+{
+	ao_context *ao = (ao_context*)ctx;
+	sdl_audio_render* sdl = NULL;
+	ao->audio_dev = (void*)(sdl = new sdl_audio_render);
+	return sdl->init_audio((void*)ao->audio_dev, channels, bits_per_sample, sample_rate, format) ? 0 : -1;
+}
+
+EXPORT_API int sdl_play_audio(void* ctx, uint8_t* data, uint32_t size)
+{
+	ao_context *ao = (ao_context*)ctx;
+	sdl_audio_render* sdl = (sdl_audio_render*)ao->audio_dev;
+	return sdl->play_audio(data, size);
+}
+
+EXPORT_API void sdl_audio_control(void* ctx, double l, double r)
+{
+	ao_context *ao = (ao_context*)ctx;
+	sdl_audio_render* sdl = (sdl_audio_render*)ao->audio_dev;
+	control_vol_t ctrl_vol = { l, r };
+	sdl->audio_control(CONTROL_SET_VOLUME, &ctrl_vol);
+}
+
+EXPORT_API void sdl_mute_set(void* ctx, int s)
+{
+	ao_context *ao = (ao_context*)ctx;
+	sdl_audio_render* sdl = (sdl_audio_render*)ao->audio_dev;
+	control_vol_t ctrl_vol;
+	ctrl_vol.mute = s;
+	sdl->audio_control(CONTROL_MUTE_SET, &ctrl_vol);
+}
+
+EXPORT_API void sdl_destory_audio(void* ctx)
+{
+	ao_context *ao = (ao_context*)ctx;
+	sdl_audio_render* sdl = (sdl_audio_render*)ao->audio_dev;
+	if (sdl)
+	{
+		sdl->destory_audio();
+		delete sdl;
+		ao->audio_dev = NULL;
+	}
+}
+
+#ifdef  __cplusplus
+}
+#endif
+
+
+bool sdl_audio_render::init_audio(void* ctx, int channels, int bits_per_sample, int sample_rate, int format)
+{
+	SDL_AudioSpec fmt;
+
+    /* Set 16-bit stereo audio at 22Khz */
+    fmt.freq = sample_rate;
+    fmt.format = AUDIO_S16;
+    fmt.channels = channels;
+    fmt.samples = 512;        /* A good value for games */
+    fmt.callback = mixaudio;
+    fmt.userdata = NULL;
+
+	SDL_OpenAudio();	
+}
---
+++ audio/sdl_render.h
@@ -0,0 +1,42 @@
+/*
+    Copyright (C) 2012  microcai <microcai@fedoraproject.org>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+
+#ifndef SDL_RENDER_H
+#define SDL_RENDER_H
+
+#include "audio_render.h"
+
+class sdl_audio_render:public audio_render
+{
+public:
+	   // 初始化音频输出.
+   virtual bool init_audio(void* ctx, int channels, int bits_per_sample, int sample_rate, int format);
+
+   // 播放音频数据.
+   virtual int play_audio(uint8_t* data, uint32_t size);
+
+   // 音频播放控制, cmd为CONTROL_开始的宏定义.
+   virtual void audio_control(int cmd, void* arg);
+
+   // 销毁音频输出组件.
+   virtual void destory_audio();
+
+};
+
+#endif // SDL_RENDER_H
