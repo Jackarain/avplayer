ab6b1f009f7a663ecd9e3fb61ad56e1def4fa9d7
Jack <jack.wgm@gmail.com>
2013-01-30 18:30:12 +0800

    add gdi render implement
    Signed-off-by: Jack <jack.wgm@gmail.com>

--- video/video_out.cpp
+++ video/video_out.cpp
@@ -5,6 +5,7 @@
 #include "ddraw_render.h"
 #include "opengl_render.h"
 #include "y4m_render.h"
+#include "soft_render.h"
 
 #ifdef  __cplusplus
 extern "C" {
@@ -151,6 +152,56 @@ EXPORT_API void ogl_destory_render(void *ctx)
 	{
 		ogl->destory_render();
 		delete ogl;
+		vo->video_dev = NULL;
+	}
+}
+
+
+EXPORT_API int gdi_init_video(void *ctx, int w, int h, int pix_fmt)
+{
+	vo_context *vo = (vo_context*)ctx;
+	soft_render *gdi = new soft_render;
+
+	vo->video_dev = (void*)gdi;
+	return gdi->init_render(vo->user_data, w, h, pix_fmt) ? 0 : -1;
+}
+
+EXPORT_API int gdi_render_one_frame(void *ctx, AVFrame* data, int pix_fmt, double pts)
+{
+	vo_context *vo = (vo_context*)ctx;
+	soft_render *gdi = (soft_render*)vo->video_dev;
+	return gdi->render_one_frame(data, pix_fmt) ? 0 : -1;
+}
+
+EXPORT_API void gdi_re_size(void *ctx, int width, int height)
+{
+	vo_context *vo = (vo_context*)ctx;
+	soft_render *gdi = (soft_render*)vo->video_dev;
+	gdi->re_size(width, height);
+}
+
+EXPORT_API void gdi_aspect_ratio(void *ctx, int srcw, int srch, int enable_aspect)
+{
+	vo_context *vo = (vo_context*)ctx;
+	soft_render *gdi = (soft_render*)vo->video_dev;
+	gdi->aspect_ratio(srcw, srch, enable_aspect);
+}
+
+EXPORT_API int gdi_use_overlay(void *ctx)
+{
+	vo_context *vo = (vo_context*)ctx;
+	soft_render *gdi = (soft_render*)vo->video_dev;
+	return gdi->use_overlay() ? 0 : -1;
+}
+
+EXPORT_API void gdi_destory_render(void *ctx)
+{
+	vo_context *vo = (vo_context*)ctx;
+	soft_render *gdi = (soft_render*)vo->video_dev;
+	if (gdi)
+	{
+		gdi->destory_render();
+		delete gdi;
 		vo->video_dev = NULL;
 	}
 }
